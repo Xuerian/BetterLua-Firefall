fe94520f5ea8025d83df4a3e82b45544f49d5067
Xuerian <sky.shell@gmail.com>
2014-09-12 05:46:58 -0400

    Comment improvements

--- betterlua_build.py
+++ betterlua_build.py
@@ -2,10 +2,6 @@ import urllib.request
 import json
 import os
 
-# sourcefile = os.path.join("..", "BetterLua", "BetterLua.YAML-tmLanguage")
-
-# assert os.path.exists(sourcefile), "Could not locate BetterLua.YAML-tmLanguage"
-
 # Fetch source syntax
 syntax_response = urllib.request.urlopen("https://raw.githubusercontent.com/Xuerian/Sublime-BetterLua/master/BetterLua.YAML-tmLanguage")
 assert syntax_response.getcode() == 200, "Could not fetch base syntax file"
@@ -14,12 +10,14 @@ assert syntax_response.getcode() == 200, "Could not fetch base syntax file"
 api_response = urllib.request.urlopen("http://firefall.nyaasync.net/Apiii/assests/production/api-production.json")
 assert api_response.getcode() == 200, "Could not fetch API json file"
 
+# Load JSON and remove info
 data = json.loads(api_response.readall().decode("utf-8"))
 del data["info"]
 
 # Extract modules and methods to form regex string
 regex = "|".join("({}\\.({}))".format(module, "|".join(str(method) for method in sorted(data[module]))) for module in sorted(data))
 
+# Construct new file
 with open("BetterLua-Firefall.YAML-tmLanguage", "w") as output:
 	for line in syntax_response:
 		output.write(line.decode("utf-8").replace("#VENDOR ", "").replace("#VENDOR", "").replace("#REGEX_VENDOR", regex).replace("#NAME_VENDOR", "firefall").replace("Lua (Better)", "Lua (Better-Firefall)"))
